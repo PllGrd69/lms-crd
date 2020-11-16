f3eb007852835541eba89e6f92522a823c9d1afa
PllGrd69 <mccalloluque@gmail.com>
2020-11-15 20:16:45 -0500

    vue

---
+++ src/components/tablita.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <button type="button" class="btn btn-primary">{{ msg }}</button>
+    <p>borrate pe</p>
+  </div>
+  
+</template>
+<script>
+export default {
+  name: 'Tablita',
+  data: function() {
+    return {
+      msg: "Tablita"
+    }
+  },   
+  methods: {
+    getMessage: function() {
+      const path = 'tabli';
+      console.log(path);
+    },
+  },
+  created: function() {
+    this.getMessage();
+  }, 
+}
+</script>
+<style>
+</style>
